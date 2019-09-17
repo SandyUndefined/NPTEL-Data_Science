1df04e82fd8c93cd51d0f73efc58bb1f1ca005e8
undefined098 <31213192+undefined098@users.noreply.github.com>
2019-09-17 21:16:46 +0530

    list completed

--- Week-2/List1.py
+++ Week-2/List1.py
@@ -17,3 +17,44 @@ print(f'Accessing list from top level component :', lists[2])
 # 2.Sub-level component
 print(f'Accessing list from sub level component :', lists[0][0])
 
+
+# Modifying a component of list
+# Using Index Value
+lists[2] = 5
+print(f'Modifying  component of list Using Index Value (top-level): ', lists)
+lists[1][0] = "Sandeep"
+print(f'Modifying  component of list Using Index Value (Sub-level): ', lists)
+
+
+# Using inbuilt function: it added element at last
+# append()
+# syntax = list_name[index].append(object)
+
+# Adding a element to a list
+lists[0].append(5)
+print(f'Adding a element to a list : ', lists)
+lists[1].append("undefined")
+print(f'Adding a element to a list : ', lists)
+
+# Adding a list to a List
+age = [22, 23, 21, 30, 25]
+lists.append(age)
+print(f'Adding a list to a List: ', lists)
+
+
+# Using insert(): it adds element to a specified position
+lists[0].insert(0, 6)
+print(lists)
+
+
+# Removing elements from a lists
+# 1. del : removes a element at a specified index number
+del lists[3]
+print(lists)
+# 2. remove() : removes the first matching element from a list.....at first occurrence
+lists[0].remove(6)
+print(lists)
+# 3. pop() : display the object that has been removed
+lists[0].pop(4)
+print(lists)
+
